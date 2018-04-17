7590cb49eaa18d82e7352c6e79710db313b68970
dalilaisan <dalila.isan@gmail.com>
2018-04-17 11:01:53 +0200

    compass activity start

---
+++ app/src/main/java/com/example/dalil/myapplication/CompassActivity.java
@@ -0,0 +1,97 @@
+package com.example.dalil.myapplication;
+
+import android.hardware.GeomagneticField;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+//this class is responsible for pointing to a given location (like a compass pointing there instead of to the north)
+
+public class CompassActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ImageView image;
+    private float currentDegree = 0f;
+    private SensorManager mSensorManager;
+    private TextView tvHeading;
+    private Location location = new Location("A");
+    private Location target = new Location("B");
+    private LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+        tvHeading = (TextView) findViewById(R.id.tvHeading);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        location.setLatitude(54.903535);
+        location.setLongitude(23.979342);
+
+        target.setLatitude(54.904618);
+        target.setLongitude(23.978782);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // get the angle around the z-axis rotated
+        float degree = Math.round(sensorEvent.values[0]);
+        degree += geoField.getDeclination();
+
+        float bearing = location.bearingTo(target);
+        degree = (bearing - degree) * -1;
+        degree = normalizeDegree(degree);
+
+        tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
+        //tvHeading.setText(String.format("Heading: %s degrees", Float.toString(degree)));
+
+        // create a rotation animation (reverse turn degree degrees)
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        // how long the animation will take place
+        ra.setDuration(210);
+
+        // set the animation after the end of the reservation status
+        ra.setFillAfter(true);
+
+        // Start the animation
+        image.startAnimation(ra);
+        currentDegree = -degree;
+    }
+
+    private float normalizeDegree(float value) {
+        if (value >= 0.0f && value <= 180.0f) {
+            return value;
+        } else {
+            return 180 + (180 + value);
+        }
+    }
+
+
+    GeomagneticField geoField;
+    private void geofield(){
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
